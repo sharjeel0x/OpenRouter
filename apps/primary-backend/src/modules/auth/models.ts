d1af56982dda1d7a1867874187b29d89d43bb9a8
sharj33l0x <sharj33lshahzad@gmail.com>
2026-02-18 01:21:48 +0500

    feat: implement authentication module with signup and signin endpoints

---
+++ apps/primary-backend/src/modules/auth/models.ts
@@ -0,0 +1,49 @@
+import { t } from 'elysia'
+
+export namespace AuthModel {
+    export const signinSchema = t.Object({
+        email: t.String(),
+        password: t.String()
+    })
+
+    export type signInSchema = typeof signinSchema.static
+
+    export const signinResponseSchema = t.Object({
+        message: t.Literal("Signed in successfully"),
+    })
+
+    export type signinResponseSchema = typeof signinResponseSchema.static;
+
+    export const signinFailureSchema = t.Object({
+        message: t.Literal("Incorrect credentials")
+    })
+
+    export type signinFailureSchema = typeof signinFailureSchema.static;
+
+    export const signupSchema = t.Object({
+        email: t.String(),
+        password: t.String()
+    })
+
+    export type signupSchema = typeof signinSchema.static
+
+    export const signupResponseSchema = t.Object({
+        id: t.String(),
+    })
+
+    export const signupFailedResponseSchema = t.Object({
+        message: t.Literal("Error while signing up")
+    })
+
+    export type signupResponseSchema = typeof signinResponseSchema.static;
+    export type signupFailedResponseSchema = typeof signupFailedResponseSchema;
+
+
+    export const profileResponseSchema = t.Object({
+        credits: t.Number()
+    })
+
+    export const profileResponseErrorSchema = t.Object({
+        message: t.Literal("Error while fetching user details")
+    })
+}
